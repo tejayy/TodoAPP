8cd55737e05aaf1f90a8aece321ffd73f0b74213
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-10-10 10:42:34 +0530

    Added States

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React, {useState} from 'react';
+
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
+  function handleChange (e) {
+    const newValue = e.target.value;
 
+    setInputText(newValue);
+  }
+
+  function addItem( ) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+  
 
   return (
     <div className="App">
       <div className="container">
-        <h1>Hello</h1>
+        <div className="heading">
+          <h1>TODO List</h1>
+        </div>
+        <div className="form">
+          <input onChange={handleChange} type="text"  value={inputText} />
+          <button onClick={addItem}>
+            <span>Add</span>
+          </button>
+        </div>
+        <div>
+          <ul>
+            {items.map((item) => <li>{item}</li>)}
+          </ul>
+        </div>
       </div>
     </div>
   );
